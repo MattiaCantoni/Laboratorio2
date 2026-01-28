7c098d8cdb92afaed20720c00dfde4e9ad70e0ca
MattiaCantoni <edc972@student.edu.ti.ch>
2026-01-28 15:42:27 +0100

    First

---
+++ LaboratorioMAUI/AppConvertitore/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace AppConvertitore
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage() // Costruttore della pagina
+        {
+            // Inizializza i componenti grafici
+            InitializeComponent();
+        }
+
+        private void btnConverti_Clicked(object sender, EventArgs e)
+        {
+            string valoreImporto = entConversione.Text;
+
+            double franchi = Convert.ToDouble(valoreImporto);
+
+            double euro = franchi * 1.07;
+
+            //lblRisultato =;
+        }
+    }
+
+}
